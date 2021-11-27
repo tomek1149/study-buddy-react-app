7cbba835cdc2ad35857db8a9b9148132ccd045c6
Tomasz Cichon <tomek1149@gmail.com>
2021-11-27 22:18:16 +0100

    used hooks

--- src/componenets/molecules/UsersListItem/UsersListItem.js
+++ src/componenets/molecules/UsersListItem/UsersListItem.js
@@ -2,17 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'componenets/atoms/Button/Button';
 import { Wrapper, StyledAverage, StyledInfo } from './UsersListItem.styles';
-
-const UsersListItem = ({ userData: { average = '0', name, attendance = '0%' } }) => {
+// przekazana funkcja do komontentu w propsach 
+const UsersListItem = ({ deleteUser, userData: { average = '0', name, attendance = '0%' } }) => {
   return (
     <Wrapper>
       <StyledAverage value={average}>{average}</StyledAverage>
       <StyledInfo>
         <p>
           {name}
-          <Button />
+          {/* wywołanie funckcji z parametrem name czyli name ma być inny od tego który jest  */}
+          <Button onClick={() => deleteUser(name)} />
         </p>
         <p>attendence: {attendance}</p>
+        <p>{average > 3.5 ? 'Good' : `should work more or no data`}</p>
       </StyledInfo>
     </Wrapper>
   );
--- src/views/Root.js
+++ src/views/Root.js
@@ -16,7 +16,7 @@ const Root = () => (
   <ThemeProvider theme={theme}>
     <GlobalStyle />
     <Wrapper>
-      <UsersList />
+      <UsersList title="User's list" />
     </Wrapper>
   </ThemeProvider>
 );
